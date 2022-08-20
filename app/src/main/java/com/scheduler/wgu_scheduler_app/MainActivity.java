7d3241c40681d08a92ad3fbc759e32700178ed0b
luke0322 <lucas.hinegardner@gmail.com>
2022-08-20 17:18:34 -0400

    Adding initialized project with fragment and view model

---
+++ app/src/main/java/com/scheduler/wgu_scheduler_app/MainActivity.java
@@ -0,0 +1,21 @@
+package com.scheduler.wgu_scheduler_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.scheduler.wgu_scheduler_app.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
